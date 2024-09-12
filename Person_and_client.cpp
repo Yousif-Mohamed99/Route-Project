20203dd52a9459b32f0a846d4b9c4bc2f52d4ef6
Yousif Mohamed <65488878+Yousif-Mohamed99@users.noreply.github.com>
2024-09-12 22:48:22 +0300

    Update Person_and_client.cpp

--- Person_and_client.cpp
+++ Person_and_client.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class Person{
-
+class Person
+{
 protected:
     int id;
     string name;
@@ -11,127 +11,126 @@ protected:
 
 public:
 
-    Person (string name, string password, int id, double balance) : id(id)
+  Person (string name, string password, int id, double balance) : id(id)
+  {
+    if(name.size() >= 5 && name.size() <= 20)
     {
-        ///set name on constructor
-        if(name.size() >= 8 && name.size() <= 20)
-        {
-            this->name = name;
-        }
-        else{
-            cout << "Error name \n";
-            cout << "Use setName to setter\n";
-        }
-        ///set password on constructor
-        if(password.size() >= 8 && password.size() <= 20)
-        {
-            this->password = password;
-        }else{
-            cout << "Error password \n";
-            cout << "Use setPassword to setter\n";
-        }
-        ///set balance on constructor
-        if(balance >1500)
-        {
-            this->balance = balance;
-        }
+      this->name = name;
     }
+    else
+    {
+      cout << "Error name \n";
+    }
+    if(password.size() >= 8 && password.size() <= 20)
+    {
+      this->password = password;
+    }
+    else
+    {
+      cout << "Error password \n";
+    }
+    if(balance > 1500 )
+    {
+      this->balance = balance;
+    }
+    else
+    {
+      cout << "Min Balance Is: 1500, Please Try Again!";
+    }
+  }
 
-    void setName (string name)
+  void setName (string name)
+  {
+    if(name.size() >= 8 && name.size() <= 20)
+    this->name = name;
+    else cout << "Min size is 5 and max is 20\n";
+  }
+  void setPassword(string password)
+  {
+    if(password.size() >= 8 && password.size() <= 20) 
+    this->password = password;
+    else cout << "Min size is 8 and max is 20\n";
+  }
+  void setBalance(double balance)
+  {
+    if(balance > 1500)
     {
-        if(name.size() >= 8 && name.size() <= 20)
-        {
-            this->name = name;
-            return;
-        }
-        cout << "Error Please enter name less 2 and more 8 \n";
-        cin >> name;
+      this->balance = balance;
     }
-    void setPassword(string password)
+    else
     {
-        if(password.size() >= 8 && password.size() <= 20)
-        {
-            this->password = password;
-        }
-        else{
-            cout << "Error Please enter name less 2 and more 8 \n";
-            cin >> password;
-        }
+      cout << "Min balance is 1500\n";
     }
-    virtual setBalance(double balance)
-    {
-        this->balance = balance;
-    }
+  }
 
-    string getName()
-    {
-        return name;
-    }
-    string getPassword()
-    {
-        return password;
-    }
-    int getID()
-    {
-        return id;
-    }
-    double getBalance()
-    {
-        return balance;
-    }
+  string getName()
+  {
+    return name;
+  }
+  string getPassword()
+  {
+    return password;
+  }
+  int getID()
+  {
+    return id;
+  }
+  double getBalance()
+  {
+    return balance;
+  }
 };
 
-class Client : public Person{
+class Client : public Person
+{
 
 public:
-    Client (string name, string password, int id, double balance) : Person(name,password,id,balance)
+  Client (string name, string password, int id, double balance) : Person(name,password,id,balance)
+  {
+  }
+  void deposit(double amount)
+  {
+    balance += amount;
+    cout << "Successful Transcation\n";
+    cout << "your balance is: " << balance << endl;
+  }
+  void withdraw (double amount)
+  {
+    if(amount < balance)
     {
+      balance -=amount;
+      cout << "Successful Transcation\n";
+      cout << "your balance is: " << balance << endl;
+      return;
     }
-
-    void deposit(double amount)
+    else cout << "Your balance is insufficient\n";
+  }
+  void transferTo (int amount, Client &recipient)
+  {
+    if (amount < balance)
     {
-        balance += amount;
-        cout << "done \n";
-        cout << "your balance is: " <<balance <<endl;
+      recipient.balance += amount;
+      balance -= amount;
+      cout << "Successful Transcation\n";
     }
-    void withdraw (double amount)
+    else
     {
-        if (amount > balance){
-            cout << "Error \n";
-            return;
-        }
-        balance -=amount;
-        cout << "done \n";
-        cout << "your balance is: " <<balance <<endl;
+      cout << "Insufficient Funds To Execute The Transaction\n";
     }
-    void transferTo (int amount, Client &recipient){
-        if (amount < balance){
-
-            balance -= amount;
-            recipient.balance += amount;
-            cout << "done \n";
-        }
-        else{
-            cout << "Error \n";
-        }
-    }
-    void checkBalance(){
-        cout << balance<<endl;
-    }
-    void display()
-    {
-        cout << name<<endl;
-        cout << id <<endl;
-        cout << balance<<endl;
-    }
+  }
+  void checkBalance()
+  {
+    cout << "Your balance is: " << balance <<endl;
+  }
+  void display()
+  {
+    cout << name<<endl;
+    cout << id <<endl;
+    cout << balance<<endl;
+  }
 };
-
 
 int main()
 {
-
-    Client c("ahmed","231",2,333333);
-
-    c.display();
-    return 0;
+  return 0;
 }
